0e284ad190d6cddf55de3cd4d4fadfec80040211
omarhossam110 <m.omar.hossam@gmail.com>
2023-12-20 04:48:46 +0200

    edit removeFav

--- src/utility/removeFav.js
+++ src/utility/removeFav.js
@@ -3,20 +3,20 @@ import { fetchData } from "../hooks/useFetch";
 
 export async function removeFav(userToken, deletedFav,saveUserToken) {
   let userData = JSON.parse(userToken);
-  console.log(userData.id)
+  // console.log(userData.id)
 
   let newUserFav
 
   await fetchData(`https://movies-app-backend-1fxl.onrender.com/users/${userData.id}`)
   .then((result) => {
       let resUserData = result.data.favorites;
-       console.log('from first fetch',resUserData)
+      //  console.log('from first fetch',resUserData)
  
         let userFav = [...resUserData];
 
         userFav = userFav.filter((fav)=> fav.id !== deletedFav.id);
 
-        console.log('after deleting:',userFav);
+        // console.log('after deleting:',userFav);
         
         newUserFav = {
           "favorites": userFav,
@@ -27,7 +27,7 @@ export async function removeFav(userToken, deletedFav,saveUserToken) {
    
    await  patchData(`https://movies-app-backend-1fxl.onrender.com/users/${userData.id}`, newUserFav).then(
       (res) => {
-        console.log("response from patch",res.data);
+        // console.log("response from patch",res.data);
         console.log("Removed from Favorite!!");
         // to update the user token in the rest of the components
         localStorage.setItem("UserInfo", JSON.stringify(res.data));
