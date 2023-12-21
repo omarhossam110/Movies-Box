f2775589f966476f5e7b010d7121cceca14a745e
omarhossam110 <m.omar.hossam@gmail.com>
2023-12-21 02:25:56 +0200

    layout edits

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,6 @@
 import React,{ useEffect,useState} from 'react';
 import { Link, useNavigate } from "react-router-dom";
 
-import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 
 import styles from "./Navbar.module.css"
