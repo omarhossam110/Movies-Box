4105f74d29a07d0c0996d0f823d5ab9e7a165239
omarhossam110 <m.omar.hossam@gmail.com>
2023-12-25 05:50:10 +0200

    responsive edits

--- src/pages/DataDetails/DataDetails.jsx
+++ src/pages/DataDetails/DataDetails.jsx
@@ -31,64 +31,72 @@ export default function DataDetails() {
     <Helmet>
         <title>{`${dataDetails?.title || dataDetails?.name} | MovieBox`}</title>
      </Helmet>
-   <section className={style.section}>
+
+   <section className={style.section} style={{height:'100h'}}>
     <div className={style.navFix}>
         
       {dataDetails ? (
 
-          <div className={`container`} style={{padding:'5rem 0'}}>
-            <div className={`row`} style={{fontSize:'1.8rem'}}>
-              
+          <div className={`container d-flex- align-items-center`} style={{padding:'5rem 0'}}>
+            
+            <div className={`row`} style={{fontSize:'1.8rem'}}>             
+              <div className="col-lg-12"> 
+                <div className={`row`}>
+                    <div className="col-lg-4">
+                        <img className="card w-100 rounded" alt={dataDetails.name || dataDetails.title}
+                            src={`https://image.tmdb.org/t/p/w500/${dataDetails.poster_path ||  dataDetails.profile_path}`}/>
+                    </div> 
+
+                    <div className={`col-lg-8 d-flex align-items-center mt-4 mt-lg-0`}>
+                    <div className={`${styles.data_container}`}>
+                            <div className={`${styles.contentDetails}`}>
+                              <div>
+                                {/* Movies / TV / Person APIs */}
+                                <h2 style={{fontSize:'3rem'}}>{dataDetails.original_title || dataDetails.name}</h2>
+                                <p>{dataDetails.tagline}</p>
+                                
+                                {/* Movies / TV  APIs */}
+                                {dataDetails.genres?.map((genre, idx) => (
+                                  <span key={idx} className="bg-light text-dark me-3  p-1 rounded">
+                                    {genre.name}
+                                  </span>
+                                ))}
+
+                                {/* Movies / TV  APIs */}
+                                {dataDetails.vote_count? <p className="mt-3">Vote Count: {dataDetails.vote_count}</p>
+                                  :null}
+
+                                {dataDetails.popularity? <p className="mt-3">Popularity: {dataDetails.popularity}</p>
+                                  :null}     
+                                
+                                {/* Movies API */}
+                                {dataDetails.release_date? <p className="mt-3">Release Date: {dataDetails.release_date}</p>
+                                  :null}
+
+                                {/* TV API */}
+                                {dataDetails.first_air_date? <h5>First Air Date: {dataDetails.first_air_date}</h5>
+                                  :null}
+                                  
+                                {/* Person API */}
+                                {dataDetails.known_for_department? <h5>Department: {dataDetails.known_for_department}</h5>
+                                  :null}
+                                
+                                {/* Movies / TV  APIs */}
+                                <p className="mt-3">{dataDetails.overview}</p>
+                              </div>
+                            </div>
+                      
+                      </div>
+                      
+                    </div> 
+                </div>
+              </div>
                        
-                <div className="col-lg-4">
-                    <img className="card w-100 rounded" alt={dataDetails.name || dataDetails.title}
-                        src={`https://image.tmdb.org/t/p/w500/${dataDetails.poster_path ||  dataDetails.profile_path}`}/>
-                </div>                   
+                                
       
 
                 
-               <div className={`col-lg-8 d-flex align-items-center`}>
-               <div className={`${styles.data_container}`}>
-                      <div className={`${styles.contentDetails}`}>
-                        <div>
-                          {/* Movies / TV / Person APIs */}
-                          <h2 style={{fontSize:'3rem'}}>{dataDetails.original_title || dataDetails.name}</h2>
-                          <p>{dataDetails.tagline}</p>
-                          
-                          {/* Movies / TV  APIs */}
-                          {dataDetails.genres?.map((genre, idx) => (
-                            <span key={idx} className="bg-light text-dark me-3  p-1 rounded">
-                              {genre.name}
-                            </span>
-                          ))}
-
-                          {/* Movies / TV  APIs */}
-                          {dataDetails.vote_count? <p className="mt-3">Vote Count: {dataDetails.vote_count}</p>
-                            :null}
-
-                          {dataDetails.popularity? <p className="mt-3">Popularity: {dataDetails.popularity}</p>
-                            :null}     
-                          
-                          {/* Movies API */}
-                          {dataDetails.release_date? <p className="mt-3">Release Date: {dataDetails.release_date}</p>
-                            :null}
-
-                          {/* TV API */}
-                          {dataDetails.first_air_date? <h5>First Air Date: {dataDetails.first_air_date}</h5>
-                            :null}
-                            
-                          {/* Person API */}
-                          {dataDetails.known_for_department? <h5>Department: {dataDetails.known_for_department}</h5>
-                            :null}
-                          
-                          {/* Movies / TV  APIs */}
-                          <p className="mt-3">{dataDetails.overview}</p>
-                        </div>
-                      </div>
-                 
-                </div>
-                
-               </div>         
+                     
                
                 
               </div>
