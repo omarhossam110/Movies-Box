43f5581781ad38346ffe2d400d04be81114f938b
omarhossam110 <m.omar.hossam@gmail.com>
2024-01-11 22:32:04 +0200

    Changed the styling of the pagination component.

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -6,11 +6,11 @@ export default function Pagination({paginate}) {
 
   return (
     <>
-      <nav aria-label="Page navigation example" className="d-flex justify-content-center my-4">
+      <nav aria-label="Page navigation example" className="d-flex justify-content-center mb-4 mt-5 ms-5">
         <ul className="pagination">
           {pageNumbers.map((number, index) => (
             <li className="page-item" key={index} style={{cursor:"pointer"}}>
-              <HashLink  to={`#P${number}`} onClick={()=> paginate(number)} className="page-link">
+              <HashLink  to={`#P${number}`} onClick={()=> paginate(number)} className="page-link" style={{fontSize:"2rem", padding:".5rem 3rem"}}>
                 {number} 
               </HashLink>
             </li>
