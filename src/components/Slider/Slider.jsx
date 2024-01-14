173fe08e32bdd487d70b730d28ae2150980f5f88
omarhossam110 <m.omar.hossam@gmail.com>
2024-01-14 09:36:26 +0200

    Update Swiper library and add CSS for responsive design

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -3,9 +3,18 @@ import React from 'react'
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
 
+
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y, EffectCoverflow } from 'swiper/modules';
+
 // Import Swiper styles
 import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
 
+import './slider.css';
 import styles from './Slider.module.css';
 import SlideCard from './SlideCard';
 
@@ -21,8 +30,21 @@ export default function Slider({sliderList,direction}) {
   return (
     <div className={`${styles.section} d-md-block d-lg-none `}>
        <Swiper
+       effect={'coverflow'}
+       grabCursor={true}
+       coverflowEffect={{
+         rotate: 50,
+         stretch: 0,
+         depth: 100,
+         modifier: 1,
+         slideShadows: true,
+        }}
+        modules={[Navigation, Pagination, Scrollbar, A11y, EffectCoverflow]}
        spaceBetween={50}
        slidesPerView={3}
+       navigation
+       pagination={{ clickable: true }}
+       scrollbar={{ draggable: true }}
        onSlideChange={() => console.log('slide change')}
        onSwiper={(swiper) => console.log(swiper)}>
        
